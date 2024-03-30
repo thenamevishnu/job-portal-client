4db2287bbda19195650e9705b043ac551f85414b
thenamevishnu <mail.vishnumk@gmail.com>
2024-03-30 21:27:55 +0530

    fixed applied list button

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
     const dispatch = useDispatch()
     const navigate = useNavigate()
-    const { id } = useSelector(state => state.user)
+    const { id, type } = useSelector(state => state.user)
 
     const logout = () => {
         dispatch(updateUser({
@@ -26,7 +26,7 @@ const Header = () => {
         <div className="h-16 flex justify-between px-5 items-center bg-gray-100 shadow shadow-black">
             <div className="font-bold text-mg">JOB PORTAL</div>
             <div>{id && <Fragment>
-                <Link to={"/applied-list"}>Applied List</Link>
+                { type === "Employer" && <Link to={"/applied-list"}>Applied List</Link>}
                 <button className="bg-red-700 ms-4 text-white px-2 p-1 rounded-xl" onClick={logout}>LogOut</button>
             </Fragment>}</div>
         </div>
